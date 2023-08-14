e1a3d8677c3bcf2eff218af5f49542ea7e951985
prachivashishtha <prachi.vashishtha@engenia.in>
2023-08-14 15:36:22 +0530

    first commit

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -5,6 +5,7 @@ async function dbConnect() {
   try {
     //const mongoURI = 'mongodb://127.0.0.1:27017/agora-db';
     const mongoURI = 'mongodb+srv://cuttingedge_agora:9AevROR6wuMPDu9V@cluster0.iu6yd.mongodb.net/cuttingedge_agora';
+    //const mongoURI = 'mongodb+srv://dextres_admin:s0GSaO2Y8d4917Q6@db-mongodb-blr1-59833-d61b8242.mongo.ondigitalocean.com/dextres?tls=true&authSource=admin&replicaSet=db-mongodb-blr1-59833';
     const opts = {
       useNewUrlParser: true,
       useUnifiedTopology: true
